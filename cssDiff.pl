3019288dd97d15998ff47f46127c1f7a4586929a
Ralph Marshall <ralph.marshall@sas.com>
2015-05-14 16:58:58 -0400

    More pruning, still not done

--- cssDiff.pl
+++ cssDiff.pl
@@ -7,10 +7,12 @@ use feature qw(say);
 use List::Util qw(pairmap);
 use Data::Dumper;
 
+my ($LOG, $OUT);
+
 sub usage($) {
     my ($errMsg) = @_;
 
-    say "ERROR: $errMsg";
+    say STDERR "ERROR: $errMsg";
     die "Exiting with extreme prejudice\n";
 }
 
@@ -47,12 +49,29 @@ sub reportNames($$$$) {
 
     pairmap {
         my ($label, @files) = ($a, @$b);
-        say "Total of $#files files for $label";
-        say join("\n", @files);
+        say $OUT "Total of $#files files for $label";
+        say $OUT join("\n", @files);
     } (
         "Version 1.22 ($oldDir)" => \@oldFiles,
         "Version 1.28 ($newDir)" => \@newFiles
     );
+}
+
+sub doPrune($$;$) {
+    my ($var, $pat, $label) = @_;
+
+    my $origVar = $var;
+
+    my $code = "\$var =~ $pat && \$&;";
+
+    my $evalRet = eval($code);
+    if ($evalRet) {
+        say $LOG "[Pruned $label] $evalRet" if $label;
+    }
+
+    die if $origVar eq $var && $evalRet;
+
+    return $var;
 }
 
 sub extractClasses(@) {
@@ -68,15 +87,17 @@ sub extractClasses(@) {
         my $wholeFile = <FILE>;
         $wholeFile =~ tr/\n\r,>+/ /;      # Convert newlines and useless punctuation to whitespace
 
-        $wholeFile =~ s@(/\*.*?\*/)@@g;  # Remove comments
-        $wholeFile =~ s/\@import.*?;//g; # Remove css import statements
-        $wholeFile =~ s/{.*?}/ /g;       # Remove body of the CSS - all we want is the selectors
-        $wholeFile =~ s/\[.*?\]//g;      # Remove attribute qualifications from selectors
-
-        $wholeFile =~ s/(\S)[.]/$1 ./g;  # Separate out all selectors that look like ".one.two" into ".one .two"
-
+        $wholeFile = doPrune($wholeFile, 's@/\*.*?\*/@@g', 'Comments');            
+        $wholeFile = doPrune($wholeFile, 's/\@import.*?;//g', 'CSS Import');       
+        $wholeFile = doPrune($wholeFile, 's/\@media.*?(?={)//g', 'Media queries');
+        $wholeFile = doPrune($wholeFile, 's/{.*?}/ /g', 'CSS Body');               # Remove body of the CSS - all we want is the selectors
+        $wholeFile = doPrune($wholeFile, 's/\[.*?\]//g', 'Attribute selector');    # Remove attribute qualifications from selectors
+        $wholeFile = doPrune($wholeFile, 's/(\S)[.]/$1 ./g');                      # Separate out all selectors that look like ".one.two" into ".one .two"
+#        $wholeFile = doPrune($wholeFile, 's/:.*?(\(.*?\))??(\b|\.)//g', 'Pseudo clases');
         my @allClasses = split(/\s+/, $wholeFile);
-        map { s/:.*// } @allClasses;     # Remove any pseudo classes
+        # map { s/:.*// } @allClasses;     # Remove any pseudo classes
+
+        map { say $LOG "[PAREN] Found $_ in $nextFile" if /^\(/ || /\)$/} @allClasses;
 
         # Stuff results for this file onto our list for all files
         push @retval, @allClasses;
@@ -103,8 +124,11 @@ sub extractClasses(@) {
 
      usage("Invalid arguments") unless $oldDir && $newDir;
 
-     say "Old root directory: $oldDir";
-     say "New root directory: $newDir";
+     open $LOG, ">cssDiff.log" || die "Unable to write log file: $!\n";
+     open $OUT, ">cssDiff.out" || die "Unable to write result file: $!\n";
+     
+     say $OUT "Old root directory: $oldDir";
+     say $OUT "New root directory: $newDir";
 
      @oldFiles = findFileNames($oldDir);
      @newFiles = findFileNames($newDir);
@@ -114,8 +138,8 @@ sub extractClasses(@) {
      @rawOldClasses = extractClasses(@oldFiles);
      @rawNewClasses = extractClasses(@newFiles);
 
-     say "List of unique selectors across all old files";
+     say $OUT "List of unique selectors across all old files";
      for (@rawOldClasses) {
-         say $_;
+         say $OUT $_;
      }
 }
